f9c8055541dd7b3716840e915706da6d22ad89ed
larry kirschner <larrykirschner@gmail.com>
2020-01-10 14:31:13 -0300

    it reads config from env

---
+++ tests/test_init_transcription_service/test_it_reads_config_from_env.py
@@ -0,0 +1,42 @@
+import os
+from unittest.mock import patch
+
+import pytest
+
+from transcribe import init_transcription_service, TranscriptionService
+
+TEST_SERVICE_ENV = {
+    "AWS_REGION": "fake-region",
+    "TRANSCRIBE_AWS_S3_BUCKET": "fake-bucket",
+    "AWS_ACCESS_KEY_ID": "fake-access-key-id",
+    "AWS_SECRET_ACCESS_KEY": "fake-secret-access-key",
+}
+
+@pytest.fixture(autouse=True)
+def after_each_clear_env():
+    for k, v in TEST_SERVICE_ENV.items():
+        os.environ[k] = v
+    yield
+    for k, v in TEST_SERVICE_ENV.items():
+        if k in os.environ:
+            del os.environ[k]
+
+
+@patch("boto3.client")
+def test_it_reads_config_from_env(mock_boto3_client):
+    service = init_transcription_service(
+        module_path="transcribe_aws"
+    )
+    assert isinstance(service, TranscriptionService)
+    mock_boto3_client.assert_any_call(
+        "s3",
+        region_name="fake-region",
+        aws_access_key_id="fake-access-key-id",
+        aws_secret_access_key="fake-secret-access-key",
+    )
+    mock_boto3_client.assert_any_call(
+        "transcribe",
+        region_name="fake-region",
+        aws_access_key_id="fake-access-key-id",
+        aws_secret_access_key="fake-secret-access-key",
+    )
