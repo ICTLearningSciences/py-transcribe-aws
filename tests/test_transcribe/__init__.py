0083eba0303d2c6fb45125b112f817bbfaef8308
larry kirschner <larrykirschner@gmail.com>
2020-01-13 18:44:34 -0300

    fixes pytest

--- tests/test_transcribe/__init__.py
+++ tests/test_transcribe/__init__.py
@@ -2,4 +2,4 @@ import pytest
 
 
 # we want to have pytest assert introspection in the helpers
-pytest.register_assert_rewrite("tests.test_aws_transcription_service.helpers")
+pytest.register_assert_rewrite("tests.test_transcribe.helpers")
